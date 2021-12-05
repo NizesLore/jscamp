3df99ce172e2344fabf1cf65cf8517aaff375c4b
NizesLore <fenciszn@hotmail.com>
2021-12-05 16:28:41 +0300

    5 December 2021 16:27 array & object destructuring

--- src/functions.js
+++ src/functions.js
@@ -1,24 +1,24 @@
-// function addToCart(quantity, productName="elma") {
-//     console.log("Sepete eklendi :" +productName+quantity)
-// }
-// //addToCart()
-//addToCart("yumurta")
-//addToCart("karpuz")
-//addToCart(10)
+function addToCart(quantity, productName="elma") {
+    console.log("Sepete eklendi :" +productName+quantity)
+}
+addToCart()
+addToCart("yumurta")
+addToCart("karpuz")
+addToCart(10)
 
 
-//ME
-// function asalMi(i =2, i<number, i++) {
-//     if (number % 2 ==0) {
-//         findPrime = false;
-//     }
-//     if (isPrime) {
-//         console.log("Sayı asaldır")
-//     } else {
-//         console.log("Sayı asal değildir")
-//     }
-// }
-// asalMi(4)
+ME
+function asalMi(i =2, i<number, i++) {
+    if (number % 2 ==0) {
+        findPrime = false;
+    }
+    if (isPrime) {
+        console.log("Sayı asaldır")
+    } else {
+        console.log("Sayı asal değildir")
+    }
+}
+asalMi(4)
 
 //string, number, boolean types 
 var teacher = "Nizes Lore"
@@ -62,6 +62,7 @@ if (hour >=5 && hour <= 11) {
 } else{
     alert ("Good night!")
 }
+
 //switch
 var language = prompt("What is your language?")
 
@@ -78,5 +79,87 @@ switch (language) {
 }
 
 
+// Arrow function
+let sayHello = () =>{
+    console.log("Hello world !")
+}
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello world2 !")
+}
+sayHello2()
+
+let product1 = {productName:"Elma", unitPrice:10,quantity:5}
+
+function addToCart3(product) {
+    console.log("Ürün :" + product.productName) 
+    console.log("Fiyat :" + product.unitPrice) 
+    console.log("Adet :" + product.quantity) 
+}
+addToCart3(product1)
+
+let product2 = {productName:"Armut", unitPrice:20,quantity:10}
+let product3 = {productName:"Kiraz", unitPrice:30,quantity:20}
+product2 = product3
+product2.productName = "KARPUZ"
+console.log(product3.productName)
+
+let sayi1 = 10
+let sayi2 = 20
+sayi1 = sayi2
+sayi2 = 100
+console.log(sayi1)
+
+function addToCart4(x) {
+    console.log(x)
+}
+let products = [
+    {productName:"Elma", unitPrice:20,quantity:10},
+    {productName:"Armut", unitPrice:20,quantity:10},
+    {productName:"Karpuz", unitPrice:20,quantity:10}
+]
+addToCart4(products)
+
+//rest
+function add(bisey,...numbers) {
+    let total = 0
+    for (let i = 0; i < numbers.length; i++) {
+        total = total + numbers[i]
+    }
+    console.log(total) 
+    console.log(bisey)
+}
+add(20,30)
+add(20,30,40)
+add(20,30,40,50)
+
+//spread
+let numbers = [30,10,500,600,120]
+console.log(...numbers)
+console.log(Math.max(...numbers))
+
+//array destructuring
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
+    {name:"İç Anadolu", population :"20M"},
+    {name:"Marmara", population :"30M"} ,
+    {name:"Karadeniz", population :"10M"},
+    [
+        ["Ankara","Konya"],
+        ["İstanbul","Bursa"],
+        ["Sinop","Trabzon"]
+    ]
+]
+console.log(icAnadolu.name)
+console.log(icAnadolu.population)
+console.log(icAnadoluSehirleri)
 
 
+//object destructuring
+let newProductName, newUnitPrice, newQuantity 
+({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } = 
+{productName:"Elma", unitPrice:20, quantity:10})
+
+console.log(newProductName)
+console.log(newUnitPrice)
+console.log(newQuantity)
