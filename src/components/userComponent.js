2dc3dc2d540f6a7c57b61c92ef4e58cc028c29cf
NizesLore <fenciszn@hotmail.com>
2021-12-24 00:55:45 +0300

    24 December -refactoring

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -12,6 +13,19 @@ let user2 = new User(2, "Baran", "Gökçekli", "Zonguldak")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
+console.log("------------------------------------------------------------------")
 
+userService.load()
+userService.getCustomersSorted()
+
+let customerToAdd = new Customer(1,"Seda","Yılmaz","Ankara","fdgdfg")
+customerToAdd.type = "customer"
+
+
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,57 @@
+//id, firstName, lastName, city, age, creditCardNumber
+export const users = [
+    {
+        id: 1,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        city:"Ankara",
+        age: 36,
+        creditCardNumber: "123456",
+        type : "customer"
+    },
+    {
+        id: 2,
+        firstName: "Nizes",
+        lastName: "Lore",
+        city: "Artvin",
+        age: 39,
+        creditCardNumber: "654321",
+        type : "customer"
+    },
+    {
+    id: 6,
+        firstName: "Birsu",
+        lastName: "Altın",
+        city: "Bolu",
+        age: 40,
+        creditCardNumber: "654321",
+        type : "customer"
+    },
+    {
+        id: 3,
+        firstName: "Sezin",
+        lastName: "Erol",
+        city: "İzmir",
+        
+        salary: 10000,
+        type : "employee"
+    },
+    {
+        id: 4,
+        firstName: "Tuana",
+        lastName: "Erol",
+        city: "İstanbul",
+        age: 20,
+        
+        type : "employee"
+    },
+    {
+        id: 5,
+        firstName: "Deniz",
+        lastName: "Beri",
+        city: "Bartın",
+        age: 20,
+       
+        type : "employe"
+    }
+]
